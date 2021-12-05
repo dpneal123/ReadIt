6e0a6bc2596d8f4e43cb118de3568e2c6fce2f31
Daniel Neal <dpneal123@gmail.com>
2021-12-05 12:07:52 +0000

    updated refresh time on votes

--- resources/views/livewire/vote.blade.php
+++ resources/views/livewire/vote.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll class="grid grid-cols-2">
+<div wire:poll.1s class="grid grid-cols-2">
     <div class="grid-span-1">
         <button wire:click="upVote({{ $post }})" id="upvote" name="upvote"
                 class="text-xl cursor-pointer mx-2 @if($post->vote->where(['isUp' => 1, 'user_id' => \Illuminate\Support\Facades\Auth::id()])->count() > 0) text-green-600 @else text-black @endif">
